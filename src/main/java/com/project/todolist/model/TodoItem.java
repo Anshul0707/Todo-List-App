84d338553b55bd2a64ba448daa0bf7517b80319f
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-07 11:15:35 +0530

    Models

---
+++ src/main/java/com/project/todolist/model/TodoItem.java
@@ -0,0 +1,16 @@
+package com.project.todolist.model;
+
+import lombok.Data;
+
+@Data
+public class TodoItem {
+    private String id;
+    private String title;
+    private String description;
+
+    public TodoItem(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+}
